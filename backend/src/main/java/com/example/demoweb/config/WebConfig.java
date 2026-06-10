3bb5df4994aab140376bb5da6afe5863530eed6e
yongku <pllpllgp@gmail.com>
2026-06-11 08:27:35 +0900

    back RENDER Front URL 크로스사이트 허용

--- backend/src/main/java/com/example/demoweb/config/WebConfig.java
+++ backend/src/main/java/com/example/demoweb/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:5173") // 리액트 개발 서버 주소
+				.allowedOrigins("http://localhost:5173", "https://frontend-server-cx28.onrender.com/")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(false);
