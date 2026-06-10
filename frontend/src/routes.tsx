581ad6738c89d201171937423a49065758cee77e
yongku <pllpllgp@gmail.com>
2026-06-10 17:17:05 +0900

    front 에러 수정

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as React from "react";
 import {createBrowserRouter, Navigate} from "react-router-dom";
 import Login from './pages/Login/Login.tsx';
